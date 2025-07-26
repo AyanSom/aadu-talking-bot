af8b2904b71b51129bdf02ad3edbb43cfce4eab5
AyanSom <32189579+AyanSom@users.noreply.github.com>
2025-07-26 23:18:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -200,4 +200,7 @@ def reset():
     return jsonify({"message": "Session reset."})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    #app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
